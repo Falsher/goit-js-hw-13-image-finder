a0b0d24d1ab16c69cd70996fbfbbcfd9c2676987
Falsher <ivan.zzaharov@gmail.com>
2021-06-04 20:36:02 +0300

    git added fuck

--- src/index.js
+++ src/index.js
@@ -12,13 +12,21 @@ let searchInput = ''
 inputSearch.addEventListener('input', debounce(valueInput, 1000))
 function valueInput (event) {
  searchInput = event.target.value;
+ console.log(searchInput)
+ imageList ()
  if(searchInput != ''){
   const button = document.createElement('button')
   button.textContent = 'Load more'
   sectorListGallery.appendChild(button)
   button.addEventListener('click', function(){page++
- console.log(page)})
+    imageList (page)})
+ }else if (searchInput != searchInput ) {
+  location.reload()
+  imageList ()
  }
+};
+
+function imageList (){
   fetch(
     `${urlPixabay}/?image_type=photo&orientation=horizontal&q=${searchInput}&page=${page}&per_page=12&key=${keyPixabay}`,
   )
@@ -33,6 +41,4 @@ function valueInput (event) {
       }
 
     });
-
-};
-
+}
